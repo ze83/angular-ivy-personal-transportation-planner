aafe6d5ea8603ede86c0628ec311f7c87ebc6fb8
ze83 <lean120483@gmail.com>
2020-10-14 17:49:00 +0200

    4 files modified, 2 added

--- src/app/transports/transports.component.ts
+++ src/app/transports/transports.component.ts
@@ -1,24 +1,28 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { TransportService } from '../services/transport.service';
-import * as fromRoot from '../shared/app.reducer';
-import { Store, State, select } from '@ngrx/store';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Observable, Subscription } from 'rxjs';
-import { Connection } from '../model/connection';
-import { selectTransports, selectFavoriteTransports, areTransportsLoaded, loading } from '../shared/transport.selectors';
-import { TransportActions } from '../shared/action-types';
-import { DatePipe } from '@angular/common';
-import { Utility } from '../shared/utility';
-import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { TransportService } from "../services/transport.service";
+import * as fromRoot from "../shared/app.reducer";
+import { Store, State, select } from "@ngrx/store";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Observable, Subscription } from "rxjs";
+import { Connection } from "../model/connection";
+import {
+  selectTransports,
+  selectFavoriteTransports,
+  areTransportsLoaded,
+  loading
+} from "../shared/transport.selectors";
+import { TransportActions } from "../shared/action-types";
+import { DatePipe } from "@angular/common";
+import { Utility } from "../shared/utility";
+import { BreakpointObserver } from "@angular/cdk/layout";
 
 @Component({
-  selector: 'app-transports',
-  templateUrl: './transports.component.html',
-  styleUrls: ['./transports.component.css']
+  selector: "app-transports",
+  templateUrl: "./transports.component.html",
+  styleUrls: ["./transports.component.css"]
 })
 export class TransportsComponent implements OnInit {
-
- @Output() onConnectionClick = new EventEmitter();
+  @Output() onConnectionClick = new EventEmitter();
 
   searchform: FormGroup;
   connections$: Observable<Connection[]>;
@@ -30,8 +34,8 @@ export class TransportsComponent implements OnInit {
   time: string;
   page = 0;
   selectedIndexTab = 0;
-  tab0 = 'normal'; //
-  tab1 = 'favorite'; //favorite
+  tab0 = "normal"; //
+  tab1 = "favorite"; //favorite
   scrollingActivated = false;
   handset = false;
   constructor(
@@ -42,15 +46,17 @@ export class TransportsComponent implements OnInit {
     private breakpointObserver: BreakpointObserver
   ) {
     this.searchform = this.fb.group({
-      from: ['Dornach', Validators.required],
-      to: ['Zurich', Validators.required],
+      from: ["", Validators.required],
+      to: ["", Validators.required],
       datetime: [new Date()],
-      time: ''
+      time: ""
     });
-    this.breakpointObserver.observe([
+    this.breakpointObserver
+      .observe([
         // Breakpoints.Handset
-        '(max-width: 455px)'
-      ]).subscribe(result => {
+        "(max-width: 455px)"
+      ])
+      .subscribe(result => {
         if (result.matches) {
           // this.activateHandsetLayout();
           // console.log('dimensione handset');
@@ -62,7 +68,6 @@ export class TransportsComponent implements OnInit {
   }
 
   ngOnInit() {
-
     this.connections$ = this.store.pipe(select(selectTransports));
     this.connections$.subscribe(connections => {
       this.connections = connections;
@@ -71,14 +76,16 @@ export class TransportsComponent implements OnInit {
       }
     });
 
-    this.favoritesConnections$ = this.store.pipe(select(selectFavoriteTransports));
+    this.favoritesConnections$ = this.store.pipe(
+      select(selectFavoriteTransports)
+    );
 
     this.resultView$ = this.store.pipe(select(areTransportsLoaded));
 
     this.loading$ = this.store.pipe(select(loading));
 
     const date = new Date();
-    this.time = this.transformTimeFormat(date.toISOString(), 'HH:mm');
+    this.time = this.transformTimeFormat(date.toISOString(), "HH:mm");
     this.searchform.controls.time.setValue(this.time);
     // const time2 = this.datepipe.transform(date, 'y-MM-dd');
     // console.log('now', this.time);
@@ -101,18 +108,20 @@ export class TransportsComponent implements OnInit {
 
   loadConnections() {
     // console.log('datetime: ', this.searchform.controls.time.value);
-    this.store.dispatch(TransportActions.loadTransportParam(
-      {
+    this.store.dispatch(
+      TransportActions.loadTransportParam({
         transport: {
-                    from: this.searchform.controls.from.value,
-                    to: this.searchform.controls.to.value,
-                    date: this.transformDateFormat(this.searchform.controls.datetime.value),
-                    time: this.searchform.controls.time.value,
-                    page: this.page
-                              // + 'T' + this.searchform.controls.time.value
-                  }
-      }
-    ));
+          from: this.searchform.controls.from.value,
+          to: this.searchform.controls.to.value,
+          date: this.transformDateFormat(
+            this.searchform.controls.datetime.value
+          ),
+          time: this.searchform.controls.time.value,
+          page: this.page
+          // + 'T' + this.searchform.controls.time.value
+        }
+      })
+    );
   }
 
   clearResults() {
@@ -132,7 +141,7 @@ export class TransportsComponent implements OnInit {
 
   loadMoreConnections() {
     this.scrollingActivated = true;
-    if (this.page <= 3 ) {
+    if (this.page <= 3) {
       this.selectedIndexTab = 0;
       this.page += 1;
       this.loadConnections();
@@ -144,12 +153,12 @@ export class TransportsComponent implements OnInit {
     console.log(this.selectedIndexTab);
   }
 
-   getContent() {
-    return document.querySelector('ion-content');
+  getContent() {
+    return document.querySelector("ion-content");
   }
 
   scrollToDown() {
-    console.log('scrollToDown');
+    console.log("scrollToDown");
     setTimeout(() => {
       if (this.getContent()) {
         this.getContent().scrollToBottom(600);
@@ -167,9 +176,6 @@ export class TransportsComponent implements OnInit {
   }
 
   transformDateFormat(date: string) {
-    return this.datepipe.transform(date, 'y-MM-dd');
+    return this.datepipe.transform(date, "y-MM-dd");
   }
-
-
-
 }
